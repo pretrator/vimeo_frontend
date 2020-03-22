0a28d889ed0b7dc6c45f0f73a1ba1f27505b8711
Ujjawl <srivastavujjawal123@gmail.com>
2020-03-22 13:16:15 +0530

    'deployed'

--- src/net/netcall.js
+++ src/net/netcall.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const getdat=()=>{
     console.log("Called Successfully")
-    return axios.get("https://starlord.hackerearth.com/bankAccount").then((r)=>{
+    return axios.get("http://starlord.hackerearth.com/bankAccount").then((r)=>{
         return r.data
     })
 }
